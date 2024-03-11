58c1032b106e11cdbde9718a60556454e6814098
Rasika <121392298+rasika-khapate@users.noreply.github.com>
2024-03-11 18:07:52 +0000

    learnt about conversions and operations

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -1,100 +1,115 @@
-// NUMBER
+// // NUMBER
 
-{
-    let score = 24;
+// {
+//     let score = 24;
 
-   // console.log(typeof score); // normal console.log  of identifier 
-    //console.log(typeof (score)); // console.log of identifer as a method    
-}
+//    // console.log(typeof score); // normal console.log  of identifier 
+//     //console.log(typeof (score)); // console.log of identifer as a method    
+// }
 
-// STRING 
+// // STRING 
   
-{
+// {
 
-    let score = "24";
+//     let score = "24";
 
-    //console.log(typeof score); // normal console.log  of identifier 
-    //console.log(typeof (score)); // console.log of identifer as a method
+//     //console.log(typeof score); // normal console.log  of identifier 
+//     //console.log(typeof (score)); // console.log of identifer as a method
 
-}
+// }
 
-// CONVSERSION OF DATATYPES
+// // CONVSERSION OF DATATYPES
 
-// 1. string to number ; use 'NUMBER'
+// // 1. string to number ; use 'NUMBER'
 
-{
-    let score = "24"
+// {
+//     let score = "24"
 
-    let valueInNumber = Number(score)
-    // console.log(valueInNumber);
-    // console.log(typeof valueInNumber);
-}
+//     let valueInNumber = Number(score)
+//     // console.log(valueInNumber);
+//     // console.log(typeof valueInNumber);
+// }
 
-// 4. number to string
+// // 4. number to string
 
-{
-    let score = 24
-    let valueInString = String(score) 
-    // console.log(valueInString);
-    // console.log(typeof valueInString);
-} 
+// {
+//     let score = 24
+//     let valueInString = String(score) 
+//     // console.log(valueInString);
+//     // console.log(typeof valueInString);
+// } 
 
-// 3. number+alphabet into number 
+// // 3. number+alphabet into number 
 
-{
-    let score = "24abc"
+// {
+//     let score = "24abc"
 
-    let valueInNumber = Number(score)
-    console.log(valueInNumber); // NaN
-    console.log(typeof valueInNumber); // number
-}
+//     let valueInNumber = Number(score)
+//     console.log(valueInNumber); // NaN
+//     console.log(typeof valueInNumber); // number
+// }
 
-// 4. number+alphabet into string
+// // 4. number+alphabet into string
 
-{
-    let score = "24abc"
+// {
+//     let score = "24abc"
 
-    let valueInNumber = String(score)
-    console.log(valueInNumber); // 24abc
-    console.log(typeof valueInNumber); // string
-}
+//     let valueInNumber = String(score)
+//     console.log(valueInNumber); // 24abc
+//     console.log(typeof valueInNumber); // string
+// }
 
-// 5. into boolean
+// // 5. into boolean
 
-{
-    let score = 1
-    let valueInString = Boolean(score) 
-    console.log(valueInString); // true
-    console.log(typeof valueInString); // boolean
-} 
+// {
+//     let score = 1
+//     let valueInString = Boolean(score) 
+//     console.log(valueInString); // true
+//     console.log(typeof valueInString); // boolean
+// } 
 
-{
-    let score = ""
-    let valueInString = Boolean(score) 
-    console.log(valueInString); // false
-    console.log(typeof valueInString); // boolean
-} 
+// {
+//     let score = ""
+//     let valueInString = Boolean(score) 
+//     console.log(valueInString); // false
+//     console.log(typeof valueInString); // boolean
+// } 
 
-{
-    let score = "Rasika"
-    let valueInString = Boolean(score) 
-    console.log(valueInString); // true
-    console.log(typeof valueInString); // boolean
-} 
+// {
+//     let score = "Rasika"
+//     let valueInString = Boolean(score) 
+//     console.log(valueInString); // true
+//     console.log(typeof valueInString); // boolean
+// } 
 
-{
-    let score = null
-    let value = Number(score) 
-    console.log(value); // 0
-    console.log(typeof value); // number
-} 
+// {
+//     let score = null
+//     let value = Number(score) 
+//     console.log(value); // 0
+//     console.log(typeof value); // number
+// } 
 
-{
-    let score = undefined
-    let value = Number(score) 
-    console.log(value); // NaN
-    console.log(typeof value); // number
-} 
+// {
+//     let score = undefined
+//     let value = Number(score) 
+//     console.log(value); // NaN
+//     console.log(typeof value); // number
+// } 
 
 
+/****************************************** OPERATIONS *****************************************/
 
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3) // 2 to the power 3
+// console.log(14 / 2) // divide to get quotient
+// console.log(3 % 2) // modulus to get remainder
+
+
+console.log(1 + "2") // 12
+console.log("1" + 2) // 12
+console.log(1 + 2 + "2") //32
+console.log("1" + 2 + 2) //122
+console.log(1 + "2" + 2) // 122
+console.log(1 + 2 + 2 + "4" + 8) //548 
